ccab8ec58414e6e944ab8100f55727a53c00bf25
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-04-15 00:39:32 +0900

    Add Advertisement component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app>
+      <v-app-bar-nav-icon />
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <v-container fluid>
+        <advertisement
+          src="//dummyimage.com/725x408/ff0000/000000&text=%E5%BA%83%E5%91%8A"
+        />
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Vue, Component } from "vue-property-decorator";
+import Advertisement from "./components/Advertisement.vue";
 
-export default Vue.extend({
-  name: "App",
-
+@Component({
   components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-});
+    Advertisement
+  }
+})
+export default class App extends Vue {}
 </script>
---
+++ src/components/Advertisement.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row dense justify="center">
+    <v-col cols="12" md="8" lg="8" xl="8">
+      <v-img :src="src" height="40vh" contain />
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Advertisement extends Vue {
+  @Prop()
+  src!: string;
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 0;
+}
+</style>
